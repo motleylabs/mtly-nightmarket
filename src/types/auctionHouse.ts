bdd65a65bc83c1614db639df2ae3a51896ee5886
metalgear-dev <dantes0121@hotmail.com>
2023-05-17 14:26:10 +0800

    yarn format

--- src/types/auctionHouse.ts
+++ src/types/auctionHouse.ts
@@ -2,19 +2,19 @@ import type { PayoutOperation } from '@motleylabs/mtly-reward-center';
 import type { PublicKey } from '@solana/web3.js';
 
 export type AuctionHouse = {
-    address: string;
-    authority: string;
-    auctionHouseFeeAccount: string;
-    auctionHouseTreasury: string;
-    sellerFeeBasisPoints: number;
-    treasuryMint: string;
-    rewardCenter: RewardCenter | null;
+  address: string;
+  authority: string;
+  auctionHouseFeeAccount: string;
+  auctionHouseTreasury: string;
+  sellerFeeBasisPoints: number;
+  treasuryMint: string;
+  rewardCenter: RewardCenter | null;
 };
 
 export type RewardCenter = {
-    address: PublicKey;
-    tokenMint: PublicKey;
-    sellerRewardPayoutBasisPoints: number;
-    payoutNumeral: number;
-    mathematicalOperand: PayoutOperation;
+  address: PublicKey;
+  tokenMint: PublicKey;
+  sellerRewardPayoutBasisPoints: number;
+  payoutNumeral: number;
+  mathematicalOperand: PayoutOperation;
 };
--- src/utils/auctionHouse.ts
+++ src/utils/auctionHouse.ts
@@ -1,73 +1,86 @@
 import { AuctionHouse as MtlyAuctionHouse } from '@motleylabs/mtly-auction-house';
 import {
-    PROGRAM_ID,
-    RewardCenter,
-    rewardCenterDiscriminator,
-  } from '@motleylabs/mtly-reward-center';
+  PROGRAM_ID,
+  RewardCenter,
+  rewardCenterDiscriminator,
+} from '@motleylabs/mtly-reward-center';
 import { bs58 } from '@project-serum/anchor/dist/cjs/utils/bytes';
-import { Connection, PublicKey } from "@solana/web3.js";
-import { AuctionHouse } from "src/types";
+import { Connection, PublicKey } from '@solana/web3.js';
+import { AuctionHouse } from 'src/types';
 
 export const getAuctionHouseByAddress = async (
-    connection: Connection,
-    auctionHouse: PublicKey
+  connection: Connection,
+  auctionHouse: PublicKey,
 ): Promise<MtlyAuctionHouse> => {
-    const mtlyHouse = await MtlyAuctionHouse.fromAccountAddress(connection, auctionHouse);
-    return mtlyHouse;
+  const mtlyHouse = await MtlyAuctionHouse.fromAccountAddress(
+    connection,
+    auctionHouse,
+  );
+  return mtlyHouse;
 };
 
-export const getAuctionHouse = async (connection: Connection, address: PublicKey): Promise<AuctionHouse | null> => {
-    let auctionHouse: AuctionHouse | null = null;
+export const getAuctionHouse = async (
+  connection: Connection,
+  address: PublicKey,
+): Promise<AuctionHouse | null> => {
+  let auctionHouse: AuctionHouse | null = null;
 
-    try {
-        const mpAuctionHouse = await getAuctionHouseByAddress(connection, address);
-    
-        auctionHouse = {
-          address: address.toBase58(),
-          authority: mpAuctionHouse.authority.toBase58(),
-          auctionHouseFeeAccount: mpAuctionHouse.auctionHouseFeeAccount.toBase58(),
-          auctionHouseTreasury: mpAuctionHouse.auctionHouseTreasury.toBase58(),
-          sellerFeeBasisPoints: mpAuctionHouse.sellerFeeBasisPoints,
-          treasuryMint: mpAuctionHouse.treasuryMint.toBase58(),
-          rewardCenter: null,
-        };
-    
-        const rewardCenterAccounts = await connection.getProgramAccounts(PROGRAM_ID, {
-          filters: [
-            {
-              memcmp: {
-                offset: 0,
-                bytes: bs58.encode(rewardCenterDiscriminator),
-              },
+  try {
+    const mpAuctionHouse = await getAuctionHouseByAddress(connection, address);
+
+    auctionHouse = {
+      address: address.toBase58(),
+      authority: mpAuctionHouse.authority.toBase58(),
+      auctionHouseFeeAccount: mpAuctionHouse.auctionHouseFeeAccount.toBase58(),
+      auctionHouseTreasury: mpAuctionHouse.auctionHouseTreasury.toBase58(),
+      sellerFeeBasisPoints: mpAuctionHouse.sellerFeeBasisPoints,
+      treasuryMint: mpAuctionHouse.treasuryMint.toBase58(),
+      rewardCenter: null,
+    };
+
+    const rewardCenterAccounts = await connection.getProgramAccounts(
+      PROGRAM_ID,
+      {
+        filters: [
+          {
+            memcmp: {
+              offset: 0,
+              bytes: bs58.encode(rewardCenterDiscriminator),
             },
-            {
-              memcmp: {
-                offset: 40,
-                bytes: address.toString(),
-              },
+          },
+          {
+            memcmp: {
+              offset: 40,
+              bytes: address.toString(),
             },
-          ],
-        });
-    
-        let rewardCenter: RewardCenter | null = null;
-    
-        if (rewardCenterAccounts.length > 0) {
-          rewardCenter = RewardCenter.deserialize(rewardCenterAccounts[0].account.data, 0)[0];
-          auctionHouse.rewardCenter = {
-            address: rewardCenterAccounts[0].pubkey,
-            tokenMint: rewardCenter.tokenMint,
-            sellerRewardPayoutBasisPoints: rewardCenter.rewardRules.sellerRewardPayoutBasisPoints,
-            payoutNumeral: rewardCenter.rewardRules.payoutNumeral,
-            mathematicalOperand: rewardCenter.rewardRules.mathematicalOperand,
-          };
-        } else {
-          auctionHouse.rewardCenter = null;
-        }
-    
-        return auctionHouse;
-    } catch (e) {
-        // eslint-disable-next-line no-console
-        console.error(e);
-        return null;
+          },
+        ],
+      },
+    );
+
+    let rewardCenter: RewardCenter | null = null;
+
+    if (rewardCenterAccounts.length > 0) {
+      rewardCenter = RewardCenter.deserialize(
+        rewardCenterAccounts[0].account.data,
+        0,
+      )[0];
+      auctionHouse.rewardCenter = {
+        address: rewardCenterAccounts[0].pubkey,
+        tokenMint: rewardCenter.tokenMint,
+        sellerRewardPayoutBasisPoints:
+          rewardCenter.rewardRules.sellerRewardPayoutBasisPoints,
+        payoutNumeral: rewardCenter.rewardRules.payoutNumeral,
+        mathematicalOperand: rewardCenter.rewardRules.mathematicalOperand,
+      };
+    } else {
+      auctionHouse.rewardCenter = null;
     }
-}
+
+    return auctionHouse;
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+    return null;
+  }
+};
