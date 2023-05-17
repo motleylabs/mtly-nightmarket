42e56fec6734c8b21befc61253cd70828ee9e5a1
metalgear-dev <dantes0121@hotmail.com>
2023-05-17 14:14:00 +0800

    initial commit

---
+++ src/types/auctionHouse.ts
@@ -0,0 +1,20 @@
+import type { PayoutOperation } from '@motleylabs/mtly-reward-center';
+import type { PublicKey } from '@solana/web3.js';
+
+export type AuctionHouse = {
+    address: string;
+    authority: string;
+    auctionHouseFeeAccount: string;
+    auctionHouseTreasury: string;
+    sellerFeeBasisPoints: number;
+    treasuryMint: string;
+    rewardCenter: RewardCenter | null;
+};
+
+export type RewardCenter = {
+    address: PublicKey;
+    tokenMint: PublicKey;
+    sellerRewardPayoutBasisPoints: number;
+    payoutNumeral: number;
+    mathematicalOperand: PayoutOperation;
+};
